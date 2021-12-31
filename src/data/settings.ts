b57310806c28c00defdecaddcb60600f3604d8dd
Sergio Cabral <git@sergiocabral.com>
2021-12-31 15:59:58 -0300

    OpenGraph

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
-    "title": "ebooks.sergiocabral.com",
-    "name": "ebooks.sergiocabral.com",
-    "description": "e-books do sergiocabral.com",
+    "title": "E-Books SERGIOCABRAL.COM",
+    "name": "E-Books SERGIOCABRAL.COM",
+    "description": "E-Books de Tecnologia",
     "url": "https://ebooks.sergiocabral.com/",
     "locale": "pt-BR",
     "browserColor": "#662277",
